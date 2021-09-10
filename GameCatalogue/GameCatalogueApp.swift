7ef19c9ecc79759ca22c9d4669c6d639b7753f8a
Rasyid <rasydridla298@gmail.com>
2021-09-10 09:42:34 +0700

    Initial Commit

---
+++ GameCatalogue/GameCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCatalogueApp.swift
+//  GameCatalogue
+//
+//  Created by Rasyid Ridla on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct GameCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
